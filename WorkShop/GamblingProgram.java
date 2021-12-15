31db9e0b499f30d3d53fd59f934046058a3e4780
SmitaDugur <smitadugur25@gmail.com>
2021-12-15 09:03:32 +0530

    Gambling game Start

--- WorkShop/GamblingProgram.java
+++ WorkShop/GamblingProgram.java
@@ -1,10 +1,16 @@
 package WorkShop;
 
 public class GamblingProgram {
+ {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  public static void main(String[] args) {
+		    {
+		       public static int gamblerAmountDay = 100;
+		        public static int gamblerHalfAmount = 50;
+		        public static int betEachGame = 1;
+		        public static int totalDays = 20;
 
-	}
+		        {
+		                GamblerBet();
 
-}
+		        }
